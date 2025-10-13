93a9c57558ea20a1e2d0430e2130978f4e3347b3
Kel Soberano <96864212+lAineDuCouteau@users.noreply.github.com>
2025-10-13 23:17:10 +0800

    Project Started

---
+++ frontend/src/page/home/home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './home.module.scss'
+
+const home = () => {
+
+  return (
+    <div className={styles.mainContainer}>vsvd</div>
+  )
+}
+
+export default home
