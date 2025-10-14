02704bfda0e8ecf6aa18fee6990dbc6491032a7b
Kel Soberano <96864212+lAineDuCouteau@users.noreply.github.com>
2025-10-14 23:15:49 +0800

    Created using .net

---
+++ MyPortfolio/Pages/Projects/Project.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyPortfolio.Pages;
+
+public class ProjectModel : PageModel
+{
+    private readonly ILogger<ProjectModel> _logger;
+
+    public ProjectModel(ILogger<ProjectModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
