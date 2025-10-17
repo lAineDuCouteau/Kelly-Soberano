628bc8978f5fc1affbb738e0c4d0c2c55d70b26e
Kel Soberano <96864212+lAineDuCouteau@users.noreply.github.com>
2025-10-17 19:11:16 +0800

    wrapper - update

--- MyPortfolio/obj/Debug/net8.0/MyPortfolio.AssemblyInfo.cs
+++ MyPortfolio/obj/Debug/net8.0/MyPortfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afe99188bfcea25d2e4659c93cb2ae45106c2985")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38e30d9b7579bc7ec318e244de4635e16d5066d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
