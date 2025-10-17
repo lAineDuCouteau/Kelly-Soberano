216a2d73d6d58e55ab49e105fe8b628c51147674
Kel Soberano <96864212+lAineDuCouteau@users.noreply.github.com>
2025-10-17 15:27:06 +0800

    project - updated

--- MyPortfolio/obj/Debug/net8.0/MyPortfolio.AssemblyInfo.cs
+++ MyPortfolio/obj/Debug/net8.0/MyPortfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1f76d4ce01e149b1d09bec3b1667868e4e81621")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+552391b4d810e8e011a869f8f3a52d713d6d1410")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
