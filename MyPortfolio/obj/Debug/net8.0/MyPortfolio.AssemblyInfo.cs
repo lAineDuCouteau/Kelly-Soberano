9fbaaddd21558ca1eb07c30e606c90617a28c3a2
Kel Soberano <96864212+lAineDuCouteau@users.noreply.github.com>
2025-10-17 17:11:12 +0800

    projetc/modal - slideshow img

--- MyPortfolio/obj/Debug/net8.0/MyPortfolio.AssemblyInfo.cs
+++ MyPortfolio/obj/Debug/net8.0/MyPortfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2922727210b1a44642dbd43f6a7a48f29c8ba310")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1051d2c730964a1fc0873e16847c388406be57b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
