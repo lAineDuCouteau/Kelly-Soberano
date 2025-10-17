e89fe1327ae97ec1507c16d15f5b017094bed3dc
Kel Soberano <96864212+lAineDuCouteau@users.noreply.github.com>
2025-10-18 02:53:50 +0800

    logo - navigate to home

--- MyPortfolio/obj/Debug/net8.0/MyPortfolio.AssemblyInfo.cs
+++ MyPortfolio/obj/Debug/net8.0/MyPortfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+886ee3203fe567c4400845a83fe8f76463ad6db7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dc20de763338f8da6ff389be37b0cc8626c1fc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
